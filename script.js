f6ac3c65aba6d6a341f6f7ae44db28382c97293c
Olgierd Krzyżaniak <olgierdkrzyzaniak@gmail.com>
2022-01-06 15:34:58 +0100

    Fetch and extract data from Github API

---
+++ script.js
@@ -0,0 +1,43 @@
+const baseEndpoint = 'https://api.github.com';
+const usersEnspoint = `${baseEndpoint}/users`;
+
+const form = document.querySelector('#form');
+const input = document.querySelector('input');
+const log = document.querySelector('#log');
+const main = document.querySelector('div.main');
+
+let reposList = []
+
+async function getReposList(username) {
+    reposList = [];
+    const response = await fetch(`${usersEnspoint}/${username}/repos`);
+    const data = await response.json();
+    console.log(data);
+    for (const result of data) {
+        const push = {
+            language: result.language,
+            resultName: result.name,
+            description: result.description,
+            url: result.html_url,
+            stars: result.stargazers_count,
+        };
+        reposList.push(push);
+    }
+    console.log(reposList)
+}
+
+function handleError(err) {
+    console.log('Error');
+    console.log(err);
+    aside.textContent = `Something went wrong: ${err}`;
+}
+
+async function logSubmit(event) {
+    event.preventDefault();
+
+    const username = input.value;
+    input.value = '';
+    await getReposList(username).catch(handleError);
+}
+
+form.addEventListener('submit', logSubmit);
