cad3b7a6c79040ff6edf0b63b473dde08310d2f1
Olgierd Krzyżaniak <olgierdkrzyzaniak@gmail.com>
2022-01-10 00:20:40 +0100

    Change to parallel API fetching

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const main = document.querySelector('div.main');
 const aside = document.querySelector('aside');
 const page = document.querySelector('#page');
 
-let reposList = [];
-let reposNum = [];
+let reposNum;
 let firstRepoOnPage;
 let lastRepoOnPage;
 let userFound = false;
+let reposList = [];
 
 function compareNumbers(a, b) {
   return b.stars - a.stars;
@@ -20,59 +20,98 @@ function compareNumbers(a, b) {
 
 async function displayUserInfo(username) {
   const response = await fetch(`${usersEnspoint}/${username}`);
-  userFound = (response.status === 200);
-  if(userFound){
+  userFound = response.status === 200;
+  if (userFound) {
     const data = await response.json();
     const html = `<aside class='bio'>
-                <a href="${data.html_url}" target="_blank"><img src="${data.avatar_url}" 
+                <a href="${data.html_url}" target="_blank"><img src="${
+      data.avatar_url
+    }" 
                 alt="${data.name} avatar"></a>
                 <section>
                     <h4>
-                        <a href="${data.html_url}" target="_blank">${data.name}</a>
+                        <a href="${data.html_url}" target="_blank">${
+      data.name
+    }</a>
                     </h4>
                     <p>${data.bio}</p>
                     <small>
                         <span>🌍 ${data.location || 'unknown'}</span>
-                        <a href="${data.blog}" target="_blank">🔗 ${data.blog || 'no blog'}</a>
+                        <a href="${data.blog}" target="_blank">🔗 ${
+      data.blog || 'no blog'
+    }</a>
                     </small>
                 </section>
             </aside>`;
     reposNum = data.public_repos;
     aside.innerHTML = html;
-    }
-}
-
-async function getReposList(username) {
-  reposList = [];
-  main.innerHTML = '<h4 style="text-align: center;">Loading...<h4>';
-  for (let i = 0; i < Math.floor(reposNum / 101) + 1; i += 1) {
-    const response = await fetch(
-      `${usersEnspoint}/${username}/repos?per_page=100&page=${i + 1}`
-    );
-    const data = await response.json();
-    for (const result of data) {
-      const push = {
-        language: result.language,
-        resultName: result.name,
-        description: result.description,
-        url: result.html_url,
-        stars: result.stargazers_count,
-      };
-      reposList.push(push);
-    }
   }
-  reposList.sort(compareNumbers);
 }
 
-function displayRepos(start, stop) {
-  displayPageNav();
-  const html = reposList
+function makeUrlList(username) {
+  const urls = [];
+  for (let i = 0; i < Math.floor(reposNum / 101) + 1; i += 1) {
+    urls.push(`${usersEnspoint}/${username}/repos?per_page=100&page=${i + 1}`);
+  }
+  return urls;
+}
+
+async function fetchRepos(urls) {
+  try {
+    const data = await Promise.all(
+      urls.map((url) =>
+        fetch(url).then((response) => response.json())
+      )
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function makeRepoList(data) {
+  const results = await data;
+  const repos = [];
+  for (const result of results.flat()) {
+    const push = {
+      language: result.language,
+      resultName: result.name,
+      description: result.description,
+      url: result.html_url,
+      stars: result.stargazers_count,
+    };
+    repos.push(push);
+  }
+  repos.sort(compareNumbers);
+  return repos;
+}
+
+function displayPageNav() {
+  if (userFound) {
+    page.innerHTML = `Repositories: <wbr>${firstRepoOnPage} - ${Math.min(
+      lastRepoOnPage,
+      reposNum
+    )}/${reposNum}`;
+    log.innerHTML = `<div class='pages'>
+      <a href="#" class="prev button">◀ Prev</a>
+      <a href="#" class="next button">Next ▶</a>
+      </div>`;
+  }
+}
+
+function displayRepos(repos, start, stop) {
+  const html = repos
     .slice(start - 1, stop)
     .map(
       (element) => `<article>
           <header class='result'>
-              <h3><a rel="bookmark" href="${element.url}" target="_blank">${element.resultName} </a></h3>
-              <small>${element.language || 'unknown'} •  ${element.stars}⭐</small>
+              <h3><a rel="bookmark" href="${element.url}" target="_blank">${
+        element.resultName
+      } </a></h3>
+              <small>${element.language || 'unknown'} •  ${
+        element.stars
+      }⭐</small>
           </header>
           <p class='description'>${element.description || 'no description'}</p>
       </article>`
@@ -88,17 +127,8 @@ function handleError(err) {
   page.innerHTML = '';
 }
 
-function displayPageNav(){
-    if(userFound){
-      page.innerHTML = `Repositories: <wbr>${firstRepoOnPage} - ${Math.min(lastRepoOnPage, reposNum)}/${reposNum}`;
-      log.innerHTML = `<div class='pages'>
-      <a href="#" class="prev button">◀ Prev</a>
-      <a href="#" class="next button">Next ▶</a>
-      </div>`;
-    }
-}
-
 async function handleSubmit(event) {
+  main.innerHTML = '<h4 style="text-align: center;">Loading...<h4>';
   log.innerHTML = '';
   event.preventDefault();
   firstRepoOnPage = 1;
@@ -106,8 +136,11 @@ async function handleSubmit(event) {
   const username = input.value;
   input.value = '';
   await displayUserInfo(username).catch(handleError);
-  await getReposList(username).catch(handleError);
-  displayRepos(firstRepoOnPage, lastRepoOnPage);
+  displayPageNav();
+  const urls = makeUrlList(username);
+  const fetchedData = fetchRepos(urls);
+  reposList = await makeRepoList(fetchedData);
+  displayRepos(reposList, firstRepoOnPage, lastRepoOnPage);
 }
 
 function pagesDelegation(event) {
@@ -116,14 +149,16 @@ function pagesDelegation(event) {
     if (lastRepoOnPage <= reposNum) {
       firstRepoOnPage += 100;
       lastRepoOnPage += 100;
-      displayRepos(firstRepoOnPage, lastRepoOnPage);
+      displayPageNav();
+      displayRepos(reposList, firstRepoOnPage, lastRepoOnPage);
     }
   }
   if (event.target.matches('.prev')) {
     if (firstRepoOnPage > 1) {
       firstRepoOnPage -= 100;
       lastRepoOnPage -= 100;
-      displayRepos(firstRepoOnPage, lastRepoOnPage);
+      displayPageNav();
+      displayRepos(reposList, firstRepoOnPage, lastRepoOnPage);
     }
   }
 }
